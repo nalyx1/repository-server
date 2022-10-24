f39ca0f0639ddb478b329de7d28addc26661b138
nalyx1 <joaorodriguez0106@gmail.com>
2022-10-24 16:39:13 -0300

    fix bcrypt bug

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,7 @@
 import * as bcrypt from 'bcryptjs';
 
-const saltRounds = 10;
-
 export const createPasswordHash = async (password: string) => {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        return bcrypt.hash(password, salt);
-    });
+    return bcrypt.hash(password, 8);
 };
 
 export const checkPassword = async (userPassword: string, password: string) => {
