382421d4f32ad92ebe2d1c3bc16809cc30d1be6f
nalyx1 <joaorodriguez0106@gmail.com>
2022-10-24 15:44:47 -0300

    add genSalt

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,11 @@
 import * as bcrypt from 'bcryptjs';
 
+const saltRounds = 10;
+
 export const createPasswordHash = async (password: string) => {
-    return bcrypt.hash(password, 8);
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        return bcrypt.hash(password, salt);
+    });
 };
 
 export const checkPassword = async (userPassword: string, password: string) => {
